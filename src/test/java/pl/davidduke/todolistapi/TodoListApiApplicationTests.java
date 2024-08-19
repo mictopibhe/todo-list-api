904ed6122d5c1f82ada2371647a9e9df920162f4
Aleksander Davydiuk <mictopibhe@gmail.com>
2024-08-19 08:43:04 +0200

    Initialization commit

---
+++ src/test/java/pl/davidduke/todolistapi/TodoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.davidduke.todolistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
