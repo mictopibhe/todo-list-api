63bb512a41ed059020b5404bb7301a67aa8ecafd
Aleksander Davydiuk <mictopibhe@gmail.com>
2024-09-04 20:08:46 +0200

    added TaskRepository

--- src/main/java/pl/davidduke/todolistapi/storage/repositories/TaskRepository.java
+++ src/main/java/pl/davidduke/todolistapi/storage/repositories/TaskRepository.java
@@ -1,7 +1,9 @@
 package pl.davidduke.todolistapi.storage.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.davidduke.todolistapi.storage.entities.TaskEntity;
 
+@Repository
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
 }
