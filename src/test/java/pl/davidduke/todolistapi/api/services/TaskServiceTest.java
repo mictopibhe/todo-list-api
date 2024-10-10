2e6cea4238a2e43ad60791bfd2843760c56e2573
Oleksandr Davydiuk <mictopibhe@gmail.com>
2024-10-10 07:20:24 +0200

    refactored AdminController

---
+++ src/test/java/pl/davidduke/todolistapi/api/services/TaskServiceTest.java
@@ -0,0 +1,28 @@
+package pl.davidduke.todolistapi.api.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskServiceTest {
+
+    @Test
+    void fetchTasks() {
+    }
+
+    @Test
+    void createTask() {
+    }
+
+    @Test
+    void fetchTaskByOwnerAndId() {
+    }
+
+    @Test
+    void updateTaskByOwnerAndId() {
+    }
+
+    @Test
+    void deleteTaskByOwnerAndId() {
+    }
+}
